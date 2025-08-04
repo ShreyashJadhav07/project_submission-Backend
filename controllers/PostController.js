2de5e2c6f3b443210cdce0feb6d0f5f84c1c7603
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-08-04 10:52:59 +0530

    update post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,36 +1,5 @@
 const PostModel = require("../Model/PostModel");
 
-
-
-// async function createPost(req,res){
-//     try{
-//         const { content } =req.body;
-//         const userId=req.userId;
-
-//         if(!content){
-//             return res.status(400).json({
-//                 message:"Post Content required"
-//             });
-//         }
-//         const newPost = await PostModel.create({
-//             content,
-//             author: userId
-//         });
-
-//         res.status(201).json({ 
-//             message: "Post Created",
-//             Post: newPost
-//         });
-
-//     } catch(err){
-//           console.error("Error in createPost:", err);
-//           res.status(500).json({ message: "Server error" });
-
-//     }
-
-
-// }
-
 async function createPost(req,res){
     try{
         // Add these debug lines
@@ -64,18 +33,6 @@ async function createPost(req,res){
           res.status(500).json({ message: "Server error" });
     }
 }
-// async function getAllPosts(req,res) {
-//     try{
-//         const posts= await PostModel.find()
-//         .populate("author", "name email") 
-//         .sort({ createdAt: -1 });
-
-//      res.status(200).json({ posts });
-//   } catch (err) {
-//     console.error("Error in getAllPosts:", err);
-//     res.status(500).json({ message: "Server error" });
-//   }
-// }
 async function getAllPosts(req,res) {
     try{
         const posts = await PostModel.find()
