92afcd629a76a31acff2d7884de3d43946984b3c
Shreyash Jadhav <shreyashjadhav913@gmail.com>
2025-08-04 10:54:48 +0530

    update api.js

--- Model/PostModel.js
+++ Model/PostModel.js
@@ -1,23 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const postSchemaRules = {
-//   content: {
-//     type: String,
-//     required: true
-//   },
-//   author: {
-//     type: mongoose.Schema.Types.ObjectId,
-//     ref: "User",
-//     required: true
-//   }
-// };
-
-// const postSchema = new mongoose.Schema(postSchemaRules, { timestamps: true });
-
-// const PostModel = mongoose.model("Post", postSchema);
-
-// module.exports = PostModel;
-
 const mongoose = require("mongoose");
 
 const postSchemaRules = {
@@ -32,9 +12,9 @@ const postSchemaRules = {
   }
 };
 
-// Make sure timestamps is enabled
+
 const postSchema = new mongoose.Schema(postSchemaRules, { 
-  timestamps: true  // This creates createdAt and updatedAt automatically
+  timestamps: true  
 });
 
 const PostModel = mongoose.model("Post", postSchema);
